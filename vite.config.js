2260b029ae83275ba8a03b687b91f0b8f690f37a
pietrocious <pietrouni@gmail.com>
2026-01-05 06:12:53 +0100

    feat: implement modern enhacements

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: '.',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html'
+    }
+  },
+  server: {
+    open: true
+  }
+})
